5e8946bc46f6c55bd8655d8411b446f66170f1a7
Alvin Kim <palifarec@gmail.com>
2022-05-05 16:48:40 -0700

    1.0.1
    Using multiple states

--- expense-management/src/App.js
+++ expense-management/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const expenses = [
     {
@@ -35,7 +35,7 @@ const App = () => {
   // Using jsx
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
